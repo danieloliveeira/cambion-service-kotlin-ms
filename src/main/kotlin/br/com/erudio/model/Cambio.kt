e0ec7a9f6c4084b4c64547d9b9eb3a75afa32d03
danielv <danielv@ciandt.com>
2026-04-27 16:33:15 -0300

    first commit

---
+++ src/main/kotlin/br/com/erudio/model/Cambio.kt
@@ -0,0 +1,23 @@
+package br.com.erudio.model
+
+import jakarta.persistence.*
+import java.math.BigDecimal
+import kotlin.jvm.Transient
+
+@Entity
+data class Cambio(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+    @Column(name = "from_currency", nullable = false, length = 3)
+    var from: String = "",
+    @Column(name = "to_currency",nullable = false, length = 3)
+    var to: String = "",
+    @Column(nullable = false)
+    var conversionFactor: BigDecimal = BigDecimal.ZERO,
+
+    @Transient
+    var convertedValue: BigDecimal = BigDecimal.ZERO,
+    @Transient
+    var environment: String? = ""
+)
